a853e019b01cf6cd62a7705e41072084341fb824
Cyanne Jones <jonescyanne@yahoo.com>
2022-08-09 09:33:42 -0600

    Display urls on page

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,16 +8,21 @@ export class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      urls: []
+      urls: [],
+      error: ""
     }
   }
 
   componentDidMount() {
+    getUrls()
+      .then(res => this.setState({urls: res.urls}))
+      .catch(error => this.setState({error: error.message}))
   }
 
   render() {
     return (
       <main className="App">
+        {this.state.error && <p>{this.state.error}</p>}
         <header>
           <h1>URL Shortener</h1>
           <UrlForm />
