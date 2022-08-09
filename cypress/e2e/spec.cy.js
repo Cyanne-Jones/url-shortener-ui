97855700a121a0f307add22403ac0e4b13edb449
Cyanne Jones <jonescyanne@yahoo.com>
2022-08-09 10:15:41 -0600

    Fix fixture error

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,12 +1,14 @@
 describe('Url Shortener', () => {
 
   beforeEach(() => {
-    cy.visit('http://localhost:3000');
+    
     cy.intercept("GET", "http://localhost:3001/api/v1/urls", {
       statusCode: 201,
       fixture: "getUrls.json"
     })
+    cy.visit('http://localhost:3000');
   });
+  
   it('Should display shortened URLS stored in server on page load', () => {
     cy.contains("h3", "Awesome photo");
     cy.contains("p", "https://images.unsplash.com/photo-1531898418865-480b7090470f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80")
